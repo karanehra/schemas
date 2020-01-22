4abbe9e9c4512d897d4c6e70049a22e9a768a693
Karan Nehra <karanehra@gmail.com>
2020-01-22 22:44:56 +0530

    Added more process methods

--- schemas.go
+++ schemas.go
@@ -4,13 +4,9 @@ import (
 	"context"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
-
-//Test defines a test struct
-type Test struct {
-	Field string
-}
 
 //Process defines a task to be sent to a processor
 type Process struct {
@@ -35,4 +31,19 @@ func GetAllProcesses(DB *mongo.Database) ([]bson.M, error) {
 func CreateProcess(DB *mongo.Database, process Process) (*mongo.InsertOneResult, error) {
 	coll := DB.Collection("process")
 	return coll.InsertOne(context.TODO(), process)
+}
+
+//GetNewProcess returns an un-intiated process
+func GetNewProcess(DB *mongo.Database) *mongo.SingleResult {
+	coll := DB.Collection("process")
+	return coll.FindOne(context.Background(), bson.M{})
+}
+
+//UpdateProcessStatus changes process status against the process with the provided ID
+func UpdateProcessStatus(DB *mongo.Database, status, processID string) (*mongo.UpdateResult, error) {
+	coll := DB.Collection("process")
+	objectID, _ := primitive.ObjectIDFromHex(processID)
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"status": status}}
+	return coll.UpdateOne(context.TODO(), filter, update)
 }
