3eca0678a562e088af093b8c8bb20fe0acd7aac0
Karan Nehra <karanehra@gmail.com>
2020-02-05 21:34:13 +0530

    Modified the get articles method

--- article.go
+++ article.go
@@ -1,5 +1,13 @@
 package schemas
 
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 //Article acts as a container for feed items. Each item comes from a
 //single Feed parent which must be defined
 type Article struct {
@@ -18,4 +26,23 @@ type Article struct {
 type ArticleExtractor struct {
 	Article
 	ID string `json:"_id" bson:"_id"`
+}
+
+//GetArticles returns feed docs
+func GetArticles(DB *mongo.Database, filter primitive.D) ([]ArticleExtractor, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	cur, err := DB.Collection("articles").Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	var results []ArticleExtractor
+	for cur.Next(ctx) {
+		var p ArticleExtractor
+		if err := cur.Decode(&p); err != nil {
+			continue
+		}
+		results = append(results, p)
+	}
+	return results, err
 }
